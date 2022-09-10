a621c4a322dba0da2595de3b08ec3d514ac24671
Anna Ivanova <genlady03@yandex.ru>
2022-09-10 18:25:19 +0300

    спринт 6 1.2

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Calm place',
+    link: 'https://images.unsplash.com/photo-1596171207381-8ecae09b5f03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    name: 'On the way',
+    link: 'https://images.unsplash.com/photo-1511173855943-aeec034188a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'New formula',
+    link: 'https://images.unsplash.com/photo-1533737338828-ebebc30718b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+  },
+  {
+    name: 'My merch',
+    link: 'https://images.unsplash.com/photo-1554454389-7bcde0df172a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'On working',
+    link: 'https://images.unsplash.com/photo-1602928298849-325cec8771c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Albuquerque',
+    link: 'https://images.unsplash.com/photo-1612212401654-e749fed6bce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,124 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile-head__edit-button_open-popup');
-let closeButton = popup.querySelector ('.popup__close');
-let popupForm = popup.querySelector('.popup__form');
-let currentName = document.querySelector('.profile-head__title');
-let currentBio = document.querySelector('.profile-head__bio');
-let nameInput = popupForm.querySelector('.popup__input-name');
-let bioInput = popupForm.querySelector('.popup__input-bio');
+const popupEdit = document.querySelector('.popup_edit');
+const editButton = document.querySelector('.profile-head__edit-button');
+const formEdit = popupEdit.querySelector('.popup__form');
+const nameInput = formEdit.querySelector('.popup__input-name');
+const bioInput = formEdit.querySelector('.popup__input-bio');
+const currentName = document.querySelector('.profile-head__title');
+const currentBio = document.querySelector('.profile-head__bio');
 
-const togglePopup = function () {
+const popupAdd = document.querySelector('.popup_add');
+const addButton = document.querySelector('.profile-head__add-button');
+const formAdd = popupAdd.querySelector('.popup__form');
+const titleInput = formAdd.querySelector('.popup__input-title');
+const linkInput = formAdd.querySelector('.popup__input-link');
 
-  popup.classList.toggle('popup_opened');
+const popupOpen = document.querySelector('.popup_open');
+const popupOpenImage = popupOpen.querySelector('.popup__image');
+const popupOpenTitle = popupOpen.querySelector('.popup__image-title');
 
+const closeButtons = document.querySelectorAll('.popup__close');
+
+const feedList = document.querySelector('.profile-feed__list');
+const cardTemplate = document.querySelector('.template').content;
+
+//ОТКРЫТИЕ-ЗАКРЫТИЕ ПОПАП
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//ФОРМА EDIT ПРИ ОТКРЫТИИ
+function openProfileEditPopup() {
+  openPopup(popupEdit);
   nameInput.value = currentName.textContent;
   bioInput.value = currentBio.textContent;
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-
-function formSubmitHandler (evt) {
+//ФУНКЦИЯ ПЕРЕЗАПИСИ EDIT
+function submitFormEditHandler(evt) {
   evt.preventDefault();
 
-  let nameInputValue = document.querySelector('.popup__input-name').value;
-  let bioInputValue = document.querySelector('.popup__input-bio').value;
-
-  currentName.getAttribute('value');
-  currentBio.getAttribute('value');
-
-  currentName.textContent = nameInputValue;
-  currentBio.textContent = bioInputValue;
-  togglePopup();
+  currentName.textContent = nameInput.value;
+  currentBio.textContent = bioInput.value;
+  closePopup(popupEdit);
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
+//ФОРМА ADD ПРИ ОТКРЫТИИ
+function openProfileAddPopup() {
+  titleInput.value = '';
+  linkInput.value = '';
+  openPopup(popupAdd);
+}
+
+//ФУНКЦИЯ СОЗДАНИЯ НОВОЙ ФОТОГРАФИИ
+function submitFormAddHandler(evt) {
+  evt.preventDefault();
+
+  const titleInputValue = titleInput.value;
+  const linkInputValue = linkInput.value;
+  addCard(titleInputValue, linkInputValue);
+  closePopup(popupAdd);
+}
+
+//ДОБАВЛЕНИЕ ФОТОГРАФИИ В НАЧАЛО
+function addCard(text, link) {
+  const newCard = printCard(text, link);
+  feedList.prepend(newCard);
+}
+
+//РАБОТА КАРТИНКИ В МОДАЛЬНОМ ОКНЕ
+function openImageHandler(src, text) {
+  openPopup(popupOpen);
+  popupOpenImage.src = src;
+  popupOpenImage.alt = text;
+  popupOpenTitle.textContent = text;
+}
+
+//СОЗДАНИЕ ТЕКУЩЕЙ ЛЕНТЫ ФОТОГРАФИЙ
+function createCurrentCards() {
+  initialCards.forEach((e) => {
+    addCard(e.name, e.link);
+  });
+}
+createCurrentCards ();
+
+//РАБОТА КАРТОЧЕК С ФОТОГРАФИЯМИ
+function printCard(text, link) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const image = cardElement.querySelector('.profile-feed__picture');
+  const imageTitle = cardElement.querySelector('.profile-feed__name');
+  const like = cardElement.querySelector('.profile-feed__like-button');
+  const trash = cardElement.querySelector('.profile-feed__trash-button');
+
+  image.src = link;
+  image.alt = text;
+  imageTitle.textContent = text;
+
+  like.addEventListener('click', () =>
+    like.classList.toggle('profile-feed__like-button_active')
+  );
+
+  trash.addEventListener('click', () =>
+    trash.closest('.card-item').remove()
+  );
+
+  image.addEventListener('click', () =>
+    openImageHandler(image.src, imageTitle.textContent)
+  );
+  return cardElement;
+}
+
+// РАБОТА КНОПКИ CLOSE
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+// СЛУШАТЕЛИ СОБЫТИЙ
+editButton.addEventListener('click', openProfileEditPopup);
+addButton.addEventListener('click', openProfileAddPopup);
+formEdit.addEventListener('submit', submitFormEditHandler);
+formAdd.addEventListener('submit', submitFormAddHandler);
